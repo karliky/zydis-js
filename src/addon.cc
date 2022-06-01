02e67b6c143d2b6a57d90be5ed618e014f1e0ece
Karliky <karliky@gmail.com>
2022-06-01 20:40:06 +0200

    simplify module

--- src/addon.cc
+++ src/addon.cc
@@ -1,30 +1,14 @@
-/*********************************************************************
- * NAN - Native Abstractions for Node.js
- *
- * Copyright (c) 2018 NAN contributors
- *
- * MIT License <https://github.com/nodejs/nan/blob/master/LICENSE.md>
- ********************************************************************/
-
 #include <nan.h>
-#include "sync.h"   // NOLINT(build/include)
-#include "async.h"  // NOLINT(build/include)
-#include "stdint.h"
-using v8::FunctionTemplate;
-using v8::Object;
-using v8::String;
-using Nan::GetFunction;
-using Nan::New;
-using Nan::Set;
+#include <Zydis/Zydis.h>
+#include <inttypes.h>
 
-// Expose synchronous and asynchronous access to our
-// Estimate() function
-NAN_MODULE_INIT(InitAll) {
-  Set(target, New<String>("calculateSync").ToLocalChecked(),
-    GetFunction(New<FunctionTemplate>(CalculateSync)).ToLocalChecked());
-
-  Set(target, New<String>("calculateAsync").ToLocalChecked(),
-    GetFunction(New<FunctionTemplate>(CalculateAsync)).ToLocalChecked());
+NAN_METHOD(getVersion) {
+  ZyanU64 version = ZydisGetVersion();
+  info.GetReturnValue().Set(Nan::New((double)version));
 }
 
-NODE_MODULE(addon, InitAll)
+NAN_MODULE_INIT(Init) {
+  NAN_EXPORT(target, getVersion);
+}
+
+NODE_MODULE(ZydisJS, Init)
